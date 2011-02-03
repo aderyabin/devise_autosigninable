18118f685fd74353aa9afe32edf419a4350e9521
Andrey Deryabin <deriabin@gmail.com>
2011-02-03 23:58:04 +0300

    added skeleton for Devise module

--- lib/devise_autosigninable.rb
+++ lib/devise_autosigninable.rb
@@ -1,1 +1,10 @@
 # DeviseAutosigninable
+require "devise"
+require 'devise_autosigninable/strategy'
+require 'devise_autosigninable/routes'
+
+Devise.add_module :autosigninable,
+#  :strategy => true,
+  :model => 'devise_autosigninable/model'
+#  :controller => :session,
+#  :route => :autosignin
