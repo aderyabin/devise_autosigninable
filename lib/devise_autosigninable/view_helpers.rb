45b16ed54594a308519f3d5e1c3f2095bd3625e5
Andrey Deryabin <deriabin@gmail.com>
2011-02-04 14:18:15 +0300

    added helper methods: link_to_autosignin and sign_in_and_redirect_to_url

--- lib/devise_autosigninable/view_helpers.rb
+++ lib/devise_autosigninable/view_helpers.rb
@@ -4,19 +4,30 @@ require 'devise/mapping'
 module Devise #:nodoc:
   module Autosigninable #:nodoc:
 
-    # OAuth2 view helpers to easily add the link to the OAuth2 connection popup and also the necessary JS code.
-    #
     module Helpers
 
-      # Creates the link to autosignin
-      def link_to_autosignin(user, link_text, options={})
-
-        link_to link_text, Devise::autosignin_path(
-            :user_id => user.id ,
-            :autosignin_token => user.autosignin_token
+      # Create the link to autosignin url based on resource with given link_text
+      # example: link_to_autosignin(user, user.email)
+      def link_to_autosignin(resource, link_text, options={})
+        link_to link_text, send("#{resource.class.to_s.downcase}_autosignin_url",
+            {:object_id => resource.id,
+            :autosignin_token => resource.autosignin_token}
           ) , options
       end
 
+      # Sign in and tries to redirect first to given url and
+      # then to the url specified by after_sign_in_path_for.
+      #
+      # If resource is blank than tries redirect to given url
+      # or root url
+      def sign_in_and_redirect_to_url(resource = nil, url=nil)
+        if resource
+          sign_in(resource)
+          redirect_to url || after_sign_in_path_for(resource)
+        else
+          redirect_to url || root_url
+        end
+      end
     end
   end
 end
