87bb217e12167ccdff86bc9dfee16b82ab0a7fe9
Andrey Deryabin <deriabin@gmail.com>
2011-02-04 23:22:53 +0300

    changed redirect back param

--- app/controllers/autosignin_controller.rb
+++ app/controllers/autosignin_controller.rb
@@ -3,11 +3,11 @@ class AutosigninController < ApplicationController
   include Devise::Autosigninable::Helpers
   
   def create
-    if resource = (authenticate(resource_name) rescue nil)
+    if resource = authenticate(resource_name)
       set_flash_message :notice, :signed_in
     else
       set_now_flash_message :alert, (warden.message || :invalid)
     end
-    sign_in_and_redirect_to_url(resource, params[:redirect_to])    
+    sign_in_and_redirect_to_url(resource, params[:return_to])    
   end
 end
