54a68a9f40e26523441b6a082352fffea15652c4
Andrey Deryabin <deriabin@gmail.com>
2011-02-04 14:16:45 +0300

    renamed route

--- lib/devise_autosigninable/routes.rb
+++ lib/devise_autosigninable/routes.rb
@@ -2,10 +2,9 @@ ActionController::Routing::RouteSet::Mapper.class_eval do
 
   protected
 
-    # Setup routes for +AutosigninSessionsController+.
-    #
+    # Setup routes for +AutosigninController+.
     def autosigninable(routes, mapping)
-      routes.autosignin ::Devise.autosignin_path, :controller => 'autosignin', :action => 'create',  :conditions => { :method => :get }
+      routes.autosignin ::Devise.autosignin_url, :controller => 'autosignin', :action => 'create',  :conditions => { :method => :get }
     end
 
 end
